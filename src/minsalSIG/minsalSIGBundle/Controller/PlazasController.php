7ce1075aee263f6b7a148110cb7942db72b5653f
marielissa <>
2014-06-18 01:10:09 -0600

    salida  tactica 3 marielissa + roberto

--- src/minsalSIG/minsalSIGBundle/Controller/PlazasController.php
+++ src/minsalSIG/minsalSIGBundle/Controller/PlazasController.php
@@ -13,7 +13,36 @@ class PlazasController extends Controller
 	 public function PlazasAction(Request $request)
     {
 		
-             return $this->render('minsalSIGminsalSIGBundle:Tactico:plazas.html.twig');
+              //para obtener los datos de la universidad
+            $repository=$this->getDoctrine();         
+            $universidad=$repository->getRepository('minsalSIGminsalSIGBundle:SsInstitucionFormadora')->findAll();
+             
+             if(count($universidad)>0){            
+                $institucion=array();            
+                foreach($universidad as $univ) 
+                {                 
+                    $institucion[$univ->getId()]= $univ->getNombre();                    
+                }                    
+            }
+             
+             //Creando el formulario
+            $form = $this->createFormBuilder(null)
+                    ->add('selectuniv', 'choice', array('choices' =>$institucion, 'empty_value' => 'Elija una Universidad'))
+                    ->add('enviar', 'submit')->getForm(); 
+            
+              $form->handleRequest($request);
+            
+            if ($form->isValid()) {
+                //recupero los Id de los select
+                $univId = $form->get('selectuniv')->getData();   
+                
+                 //Recuperar el id de ss_carrera_inst 
+               $varCarrInst = $repository->getRepository('minsalSIGminsalSIGBundle:SsCarreraInstForm')->findBy(array('idInstitucionFormadora' => $univId));
+                
+            
+            }
+            
+             return $this->render('minsalSIGminsalSIGBundle:Tactico:plazas.html.twig', array('form' => $form->createView(), 'universidad' => $universidad,));
 
      }
 }
